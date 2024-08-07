320a0f710973313e5768b00ec98a7a9baa6698d5
harshuCodes-git <1032210414@tcetmumbai.in>
2024-08-07 08:48:23 +0530

    Update index.jsx

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import GooglePlacesAutocomplete from "react-google-autocomplete";
 import { Input } from "@/components/ui/input";
 import { SelectBudget, SelectTravelList } from "@/constants/option";
 import { Button } from "@/components/ui/button";
+
 const CreateTrip = () => {
   const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
   const [place, setPlace] = useState(null);
+  const [formData, setFormData] = useState({});
+
+  const handleInputChange = (name, value) => {
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  useEffect(() => {
+    console.log(formData);
+  }, [formData]);
 
   return (
     <div className="container">
       <div className="sm:px-10 md:px-32 lg:px-56 xl:px-72 px-8 mt-10">
-        <h2 className="font-bold text-3xl">What you have to do ?</h2>
+        <h2 className="font-bold text-3xl">What you have to do?</h2>
         <p className="text-gray-500">
-          Just provide basic information, our trip planner help you to find the
+          Just provide basic information, our trip planner helps you to find the
           best place. <br />
-          We make sure that your journey will be full of JOY, Adventure,
+          We make sure that your journey will be full of JOY and Adventure.
         </p>
         <div className="mt-[30px]">
           <div>
@@ -26,15 +39,19 @@ const CreateTrip = () => {
                 apiKey={apiKey}
                 onPlaceSelected={(place) => {
                   setPlace(place);
-                  console.log(place);
+                  handleInputChange("destination", place);
                 }}
               />
             </div>
             <div>
               <h2 className="text-xl my-3 font-medium">
-                For how many days you are planning?
+                For how many days are you planning?
               </h2>
-              <Input placeholder={"Ex-5"} type="number" />
+              <Input
+                placeholder={"Ex-5"}
+                type="number"
+                onChange={(e) => handleInputChange("noOfDays", e.target.value)}
+              />
             </div>
             <div>
               <h2 className="text-xl my-3 font-medium">
@@ -44,33 +61,16 @@ const CreateTrip = () => {
                 {SelectBudget.map((budget) => (
                   <div
                     key={budget.id}
-                    className="p-4 border rounded-lg justify-center align-center  hover:shadow-lg "
+                    className={`p-4 border rounded-lg justify-center align-center hover:shadow-lg ${
+                      formData.budget === budget.title &&
+                      "shadow-lg border-black"
+                    }`}
+                    onClick={() => handleInputChange("budget", budget.title)}
                   >
                     <img
                       src={budget.icon}
                       alt={budget.title}
-                      className="w-12 h-12 object-cover text-4xl "
-                    />
-                    <h3 className="font-bold text-lg">{budget.title}</h3>
-                    <p className="text-sm text-gray-500">
-                      {budget.description}
-                    </p>
-                  </div>
-                ))}
-              </div>
-              <h2 className="text-xl my-3 font-medium">
-                You are Planning with Whom?
-              </h2>
-              <div className="grid grid-cols-3 gap-5 mt-4">
-                {SelectTravelList.map((budget) => (
-                  <div
-                    key={budget.id}
-                    className="p-4 border rounded-lg justify-center align-center  hover:shadow-lg "
-                  >
-                    <img
-                      src={budget.icon}
-                      alt={budget.title}
-                      className="w-10 h-10 object-cover text-2xl "
+                      className="w-12 h-12 object-cover text-4xl"
                     />
                     <h3 className="font-bold text-lg">{budget.title}</h3>
                     <p className="text-sm text-gray-500">
@@ -80,9 +80,36 @@ const CreateTrip = () => {
                 ))}
               </div>
             </div>
+            <div>
+              <h2 className="text-xl my-3 font-medium">
+                You are Planning with Whom?
+              </h2>
+              <div className="grid grid-cols-3 gap-5 mt-4">
+                {SelectTravelList.map((travel) => (
+                  <div
+                    key={travel.id}
+                    className={`p-4 border rounded-lg justify-center align-center hover:shadow-lg ${
+                      formData.travel === travel.title &&
+                      "shadow-lg border-black"
+                    }`}
+                    onClick={() => handleInputChange("travel", travel.title)}
+                  >
+                    <img
+                      src={travel.icon}
+                      alt={travel.title}
+                      className="w-10 h-10 object-cover text-2xl"
+                    />
+                    <h3 className="font-bold text-lg">{travel.title}</h3>
+                    <p className="text-sm text-gray-500">
+                      {travel.description}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
-        <div className="mt-4  mb-16 flex justify-end">
+        <div className="mt-4 mb-16 flex justify-end">
           <Button>Generate Trip!🗺️</Button>
         </div>
       </div>
